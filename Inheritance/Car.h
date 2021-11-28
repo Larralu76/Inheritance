25074cfae42b0d784aced87ff9ffcf121de21e8f
larralu <larralu@aol.com>
2021-11-28 18:20:33 -0500

    Completed Car.h

--- Inheritance/Car.h
+++ Inheritance/Car.h
@@ -1,1 +1,24 @@
 #pragma once
+
+#ifndef CAR_H_
+#define CAR_H_
+
+#include <iostream>
+#include <string>
+#include "Vehicle.h"
+
+using namespace std;
+
+class Car : public Vehicle
+{
+	int num_doors;
+
+public:
+	Car();
+	Car(string manny, int yr, int doors);
+	int getDoors();
+	void setDoors(int doors);
+	void displayInfo();
+};
+
+#endif
