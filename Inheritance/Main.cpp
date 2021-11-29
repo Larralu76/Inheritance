bad39fc628136f37ff586c7eb2b69c78d109f1e5
larralu <larralu@aol.com>
2021-11-28 21:20:35 -0500

    completed Main.cpp

---
+++ Inheritance/Main.cpp
@@ -0,0 +1,66 @@
+//Laura Luke
+//Novemeber 28, 2021
+//CIS 1202 501
+//Inheritence
+
+#include <iostream>
+#include <string>
+#include "Vehicle.h"
+#include "Car.h"
+#include "Truck.h"
+
+using namespace std;
+
+int main()
+{
+	string manny;
+	int yr;
+	int doors;
+	int towingCap;
+
+	cout << "Vehicle Program: " << endl;
+	cout << endl;
+	cout << "Vehicle: " << endl;
+
+	cout << "Enter the Manufacturer: ";
+	getline(cin, manny);
+
+	cout << "Enter the year built: ";
+	cin >> yr;
+
+	Vehicle vehicle(manny, yr);
+	vehicle.displayInfo();
+	cout << endl;
+	cout << "Car: " << endl;
+	cin.clear();
+	cin.ignore(100, '\n');
+
+	cout << "Enter the Manfacturer: " << endl;
+	getline(cin, manny);
+
+	cout << "Enter the Year Built: " << endl;
+	cin >> yr;
+	
+	cout << "Enter the number of doors: ";
+	cin >> doors;
+
+	Car car(manny, yr, doors);
+	car.displayInfo();
+	cout << endl << "Truck: " << endl;
+	cin.clear();
+	cin.ignore(100, '\n');
+
+	cout << "Enter the Manfacturer: " << endl;
+	getline(cin, manny);
+
+	cout << "Enter the Year Built: " << endl;
+	cin >> yr;
+
+	cout << "Enter the Towing Capacity: ";
+	cin >> towingCap;
+
+	Truck truck(manny, yr, towingCap);
+	truck.displayInfo();
+
+	return 0;
+}
