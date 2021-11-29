1a23a8222bb4a860cff58f35c72a681a2e3b94e4
larralu <larralu@aol.com>
2021-11-28 19:50:01 -0500

    Completed truck.h

--- Inheritance/Truck.h
+++ Inheritance/Truck.h
@@ -1,1 +1,24 @@
 #pragma once
+
+#ifndef TRUCK_H_
+#define TRUCK_H_
+#include <iostream>
+#include <string>
+#include "Vehicle.h"
+
+using namespace std;
+
+class Truck:public Vehicle
+{
+	int towingCap;
+
+public:
+
+	Truck();
+	Truck(string manny, int yr, int tow);
+	int getTowCap();
+	void setTowCap(int setT);
+	void displayInfo();
+};
+
+#endif
